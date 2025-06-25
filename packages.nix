38dd5c8f645a027cd43da0705adfc52f46cef4a3
Victor R <39545521+viicslen@users.noreply.github.com>
2025-06-25 09:42:54 -0400

    Refactor packages.nix to improve structure and allow unfree packages for Neovim configuration

--- packages.nix
+++ packages.nix
@@ -5,17 +5,35 @@
 } @ args: {
   perSystem = {
     config,
+    system,
     pkgs,
     lib,
     ...
-  }: {
+  }: let
+    # Create nixpkgs with unfree allowed for nvf
+    pkgsUnfree = import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      # Or be more specific:
+      # config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+      #   "intelephense"
+      # ];
+    };
+  in {
     packages = {
-      laravel-nvim = pkgs.callPackage ./pkgs/laravel-nvim.nix { };
-      neotest-pest = pkgs.callPackage ./pkgs/neotest-pest.nix { };
+      laravel-nvim = pkgs.callPackage ./pkgs/laravel-nvim.nix {};
+      neotest-pest = pkgs.callPackage ./pkgs/neotest-pest.nix {};
 
-      default = (inputs.nvf.lib.neovimConfiguration {
-          inherit pkgs;
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = pkgsUnfree;
           modules = [
+            {
+              # Pass the custom packages to the nvf configuration
+              _module.args = {
+                inherit (config.packages) laravel-nvim neotest-pest;
+              };
+            }
             ./config
           ];
         })
