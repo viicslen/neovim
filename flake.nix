5a27492b864e9e18c65ce3aa87b5d3d51298938e
Victor R <39545521+viicslen@users.noreply.github.com>
2025-06-25 09:42:45 -0400

    Refactor flake.nix for improved readability and structure

--- flake.nix
+++ flake.nix
@@ -5,30 +5,44 @@
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = inputs@{ flake-parts, self, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
-    imports = [
-      ./apps.nix
-      ./packages.nix
-    ];
-    systems = [
-      "x86_64-linux"
-    ];
-    perSystem = { config, pkgs, ... }: {
-      # Provide the default formatter. `nix fmt` in project root
-      # will format available files with the correct formatter.
-      # P.S: Please do not format with nixfmt! It messes with many
-      # syntax elements and results in unreadable code.
-      formatter = pkgs.alejandra;
+  outputs = inputs @ {
+    flake-parts,
+    self,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} (top @ {
+      config,
+      withSystem,
+      moduleWithSystem,
+      ...
+    }: {
+      imports = [
+        ./apps.nix
+        ./packages.nix
+      ];
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        # Provide the default formatter. `nix fmt` in project root
+        # will format available files with the correct formatter.
+        # P.S: Please do not format with nixfmt! It messes with many
+        # syntax elements and results in unreadable code.
+        formatter = pkgs.alejandra;
 
-      # Check if codebase is properly formatted.
-      # This can be initiated with `nix build .#checks.<system>.nix-fmt`
-      # or with `nix flake check`
-      checks = {
-        nix-fmt = pkgs.runCommand "nix-fmt-check" {nativeBuildInputs = [pkgs.alejandra];} ''
-          alejandra --check ${self} < /dev/null | tee $out
-        '';
+        # Check if codebase is properly formatted.
+        # This can be initiated with `nix build .#checks.<system>.nix-fmt`
+        # or with `nix flake check`
+        checks = {
+          nix-fmt = pkgs.runCommand "nix-fmt-check" {nativeBuildInputs = [pkgs.alejandra];} ''
+            alejandra --check ${self} < /dev/null | tee $out
+          '';
+        };
       };
-    };
-  });
+      debug = true;
+    });
 }
